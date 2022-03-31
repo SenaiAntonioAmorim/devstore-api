7ee0da4ae0c77ae38f00bf2946c1a60f5b0af857
SenaiAntonioAmorimemail <lotusprime@outlook.com.br>
2022-03-31 18:12:39 -0300

    commit inicial

---
+++ src/main/java/com/amorim/devstore/DevstoreApplication.java
@@ -0,0 +1,13 @@
+package com.amorim.devstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstoreApplication.class, args);
+	}
+
+}
