f711784665a755fc3fbf4f8853802a887e9a2247
SenaiAntonioAmorimemail <lotusprime@outlook.com.br>
2022-03-31 19:06:19 -0300

    repositories implementados

--- src/main/java/com/amorim/devstore/domain/Categoria.java
+++ src/main/java/com/amorim/devstore/domain/Categoria.java
@@ -1,19 +1,28 @@
 package com.amorim.devstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 
-public class Categoria {
+public class Categoria implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
     private List<Parts>parts = new ArrayList<>();
 
 	public Categoria() {
---
+++ src/main/java/com/amorim/devstore/repository/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.amorim.devstore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.amorim.devstore.domain.Categoria;
+
+@Repository
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
